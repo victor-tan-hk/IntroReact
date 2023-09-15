944408817d4feddf35bcbeddc88268ac333e4d13
Victor Tan <victor.tan.33@gmail.com>
2023-09-15 13:59:53 +0800

    second update to code cafe app along with manual

---
+++ labcode/code-cafe-app/App-v16.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useEffect, useReducer, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import DetailItem from './components/DetailItem';
+import Details from './components/Details';
+import Header from './components/Header';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import { cartReducer, initialCartState } from './reducers/cartReducer';
+
+function App() {
+  
+  const [items, setItems] = useState([]);
+  const [cart, dispatch] = useReducer(cartReducer, initialCartState);
+
+  useEffect(() => {
+    axios.get('/api/items')
+      .then((result) => setItems(result.data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <Router>
+      <Header />
+      {items.length === 0
+        ? <div>Loading...</div>
+        : (
+          <Routes>
+
+            <Route path="/details" element={<Details items={items} />}>
+              <Route path=":id" element={<DetailItem items={items}/>} />
+              <Route index element={<div>No Item Selected</div>} />
+            </Route>
+
+            <Route path="/" element={<Home items={items} />} />
+            <Route path="*" element={<NotFound/>} />
+          </Routes>
+        )}
+    </Router>
+  );
+}
+
+export default App;
---
+++ labcode/code-cafe-app/Cart-v20.js
@@ -0,0 +1,11 @@
+import './Cart.css';
+
+function Cart() {
+    return (
+        <div className="cart-component">
+        <h2>Your Cart</h2>
+        </div>
+    );
+}
+
+export default Cart;
